7718a2ce9319114e02ded328589a302ad50a8453
Andrea Piano <andreapiano@Andreas-MacBook-Pro.local>
2023-09-26 17:42:05 +0100

    feat: added xmas deadline in header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -5,6 +5,15 @@ import "./Header.css";
 import DarkMode from "./DarkMode/DarkMode";
 
 const Header = () => {
+  const today = new Date();
+
+  const christmas = new Date(today.getFullYear(), 11, 25);
+
+  if (today > christmas) {
+    christmas.setFullYear(today.getFullYear() + 1);
+  }
+
+  const daysLeft = Math.ceil((christmas - today) / (1000 * 60 * 60 * 24));
   return (
     <>
       <header className="header">
@@ -19,11 +28,11 @@ const Header = () => {
 
       <div className="opening">
         <h3 aria-label="Your destination" title="Your destination">
-          "Ho, Ho Ho! Christmas is coming soon!
+          "Ho, Ho Ho! Christmas is just <span>{daysLeft} days</span> away!
         </h3>
         <h3 aria-label="Your trip title" title="Your trip title">
           {" "}
-          Stuck on what to give your loved ones? <span>Fear not!</span>
+          Stuck on what to give your loved ones? Fear not!
         </h3>
         <h3>
           {" "}
